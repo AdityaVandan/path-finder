8518679081670a832a5f68fe919ac428ac8effe6
AdityaVandan <van13heisenberg@gmail.com>
2020-04-17 08:31:16 +0530

    Updates

---
+++ precache-manifest.66d293ff5c740d675126b9c82642e43a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca482e8c2096802e0c4be3eb9f378fb7",
+    "url": "/path-finder/index.html"
+  },
+  {
+    "revision": "fef81f4c9038c4e0649f",
+    "url": "/path-finder/static/css/main.0735ea48.chunk.css"
+  },
+  {
+    "revision": "7cd48ed5c4f9698c2b3d",
+    "url": "/path-finder/static/js/2.5cb6aaac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/path-finder/static/js/2.5cb6aaac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef81f4c9038c4e0649f",
+    "url": "/path-finder/static/js/main.daf85c76.chunk.js"
+  },
+  {
+    "revision": "d225e96069fc4eff9cb3",
+    "url": "/path-finder/static/js/runtime-main.4bb9a73f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/path-finder/static/media/logo.5d5d9eef.svg"
+  }
+]);
